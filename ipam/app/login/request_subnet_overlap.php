8bfab45ca0418d739368fc2af0ab8bdd49bd7dda
Andrea Cristalli <acs.cluster@gmail.com>
2016-08-23 10:15:59 +0200

    Corrected stop overlap check in case of invalid value

--- ipam/app/login/request_subnet_overlap.php
+++ ipam/app/login/request_subnet_overlap.php
@@ -21,65 +21,66 @@ if (strlen($cidr_check) > 5) {
     $errors[] = $cidr_check;
 }
 // check Overlap
-$overlap = $Subnets->verify_subnet_overlapping($sectionId, $new_subnet, $vrfId = 0);
-if ($overlap !== false) {
-    $errors[] = $overlap;
+if (! sizeof(@$errors) > 0) {
+    $overlap = $Subnets->verify_subnet_overlapping($sectionId, $new_subnet, $vrfId = 0);
+    if ($overlap !== false) {
+        $errors[] = $overlap;
+        $subnet = $Tools->transform_to_decimal($_POST['subnet']);
+        $show = $Tools->fetch_object('subnets', 'subnet', $subnet);
+    }
 }
 
 /* If no errors are present execute request */
 if (sizeof(@$errors) > 0) {
-    $subnet = $Tools->transform_to_decimal($_POST['subnet']);
-    $show = $Tools->fetch_object('subnets', 'subnet', $subnet);
-
     print '<div class="modal fade" id="overlap_details" tabindex="-1" role="dialog" aria-labelledby="overlap_details">';
-        print '<div class="modal-dialog" role="document">';
-            print '<div class="modal-content">';
-                print '<div class="modal-header  modal-header-info">';
-                    print '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
-                    print '<h4 class="modal-title" id="myModalLabel">Errors details</h4>';
-                print '</div>';
-                print '<div class="modal-body">';
-                    print '<div class="alert alert-danger"><strong>' . _('Please fix following problems') . '</strong>:';
-                        foreach ($errors as $error) {
-                            print "<br>" . $error;
-                        }
-                     print '</div>';
-                    if ($subnet !=0) {
-                    print '<table class="table table-responsive table-auto-wide">';
-                        print '<tr>';
-                            print '<th>Subnet</th>';
-                            print '<td>'.$Tools->transform_to_dotted($show->subnet).'/'.$show->mask.'</td>';
-                        print '</tr>';
-                        print '<tr>';
-                            print '<th>System Name</th>';
-                            print '<td>'.$show->{"System Name"}.'</td>';
-                        print '</tr>';
-                        print '<tr>';
-                            print '<th>Description</th>';
-                            print '<td>'.$show->description.'</td>';
-                        print '</tr>';
-                        print '<tr>';
-                            print '<th>Customer</th>';
-                            print '<td>'.$show->Account.'</td>';
-                        print '</tr>';
-                        print '<tr>';
-                            print '<th>Site</th>';
-                            print '<td>'.$show->Site.'</td>';
-                        print '</tr>';
-                        print '<tr>';
-                            print '<th>Comments</th>';
-                            print '<td>'.$show->Comments.'</td>';
-                        print '</tr>';
-                    print '</table>';
-                    }
-                print '</div>';
-                print '<div class="modal-footer">';
-                    print '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>';
-               print ' </div>';
-            print '</div>';
-        print '</div>';
+    print '<div class="modal-dialog" role="document">';
+    print '<div class="modal-content">';
+    print '<div class="modal-header  modal-header-info">';
+    print '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+    print '<h4 class="modal-title" id="myModalLabel">Errors details</h4>';
     print '</div>';
-   die();
+    print '<div class="modal-body">';
+    print '<div class="alert alert-danger"><strong>' . _('Please fix following problems') . '</strong>:';
+    foreach ($errors as $error) {
+        print "<br>" . $error;
+    }
+    print '</div>';
+    if ($subnet != 0) {
+        print '<table class="table table-responsive table-auto-wide">';
+        print '<tr>';
+        print '<th>Subnet</th>';
+        print '<td>' . $Tools->transform_to_dotted($show->subnet) . '/' . $show->mask . '</td>';
+        print '</tr>';
+        print '<tr>';
+        print '<th>System Name</th>';
+        print '<td>' . $show->{"System Name"} . '</td>';
+        print '</tr>';
+        print '<tr>';
+        print '<th>Description</th>';
+        print '<td>' . $show->description . '</td>';
+        print '</tr>';
+        print '<tr>';
+        print '<th>Customer</th>';
+        print '<td>' . $show->Account . '</td>';
+        print '</tr>';
+        print '<tr>';
+        print '<th>Site</th>';
+        print '<td>' . $show->Site . '</td>';
+        print '</tr>';
+        print '<tr>';
+        print '<th>Comments</th>';
+        print '<td>' . $show->Comments . '</td>';
+        print '</tr>';
+        print '</table>';
+    }
+    print '</div>';
+    print '<div class="modal-footer">';
+    print '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>';
+    print ' </div>';
+    print '</div>';
+    print '</div>';
+    print '</div>';
+    die();
 }
 
 //print $overlap;
