109189370c102ce9cbc212ee15b1ad8511a7e12a
Andrea Cristalli <acs.cluster@gmail.com>
2016-09-09 16:58:27 +0200

    Updated puppet install page

--- provisioning/templates/puppetInstall.tpl.php
+++ provisioning/templates/puppetInstall.tpl.php
@@ -5,24 +5,14 @@ $this->assign('nav', 'instpup');
 $this->display('_Header.tpl.php');
 ?>
 <link href="bootstrap/css/jquery-labelauty.css" rel="stylesheet" />
-<style>
-    body #modalMonitor {
-        /* new custom width */
-        width: 100%;
-        /* must be half of the width, minus scrollbar on the left (30px) */
-        margin-left: -50%;
-    }
-</style>
+
 <script>
 
     $(document).ready(function () {
-        $('#details').on('click', function () {
-            $('#basicModal').show();
-        });
+
         $('.release').hide();
         $('#msg').hide();
         $('.network').hide();
-        $('#disable').removeAttr('href');
         $('#release').on('change', function () {
             var release = $(this).val();
             if (release === '') {
@@ -80,25 +70,22 @@ $this->display('_Header.tpl.php');
             $('.release').show();
             SO = SOarr[1].trim();
             var ACR = SOarr[2].trim();
-            $.get("/SPOT/provisioning/api/tblprogresses?salesorder=" + SO, function (jsonResult) {
+
+             $.get("/SPOT/provisioning/api/tblprogresses?salesorder=" + SO, function (jsonResult) {
                 $('.network').show();
                 var Jdata = jsonResult.rows[0].data;
                 var Jsonspecs = JSON.parse(Jdata);
                 $.each(Jsonspecs.clients, function (i, o) {
 
                     if (o.gateway !== '') {
-                        var ipaddress = o.gateway;
-                        $('#ipaddress').val(ipaddress);
+                        var gateway = o.gateway;
+                        $('#gateway').val(gateway);
                     }
-                    if (o.netmask !== '') {
-                        var netmask = o.netmask;
-                        $('#netmask').val(netmask);
-                    }
+                   
                 });
             });
-
         });
-        $(document).on('click', '#start', function (e) {
+        $(document).on('click', '#install', function (e) {
 
             if ($('*[required]').val() === '') {
 
@@ -114,69 +101,20 @@ $this->display('_Header.tpl.php');
                 $('#msg').hide();
                 validate(e);
             }
-            $('#start').after("<button class='pull-right btn btn-primary' id='Results'>Get Results !</button>");
-            $('#start').remove();
-            $('input[type=checkbox]').prop('disabled', 'true');
-            $('.uncheck').remove();
-            var ipaddress = " -ip " + $('#ipaddress').val();
-            var netmask = " -n " + $('#netmask').val();
-            var ipaliasID = 7;
+
+            $('#install').attr('disabled', 'disabled');
+
+            // return false
+            var ipaddress = " -i " + $('#ipaddress').val();
+            var gateway = " -g " + $('#gateway').val();
+            var release = " -r " + $('#release').val();
             var datastring;
             var args = {
                 "0": ipaddress,
-                "1": netmask,
+                "1": gateway,
+                "2": release
             };
-            var argsIP = JSON.stringify(args);
-            //  validate(e);
-            var input;
-            var hosts = [];
-            var keys = [];
-
-
-            $('.items').each(function () {
-                var modules;
-                var install;
-                var options;
-                input = $(this).find(' input.ipaddress');
-                var host = "-H " + input.val();
-                var hostId = input.attr('id');
-                var index = $('#' + hostId + '_index').val();
-                for (var i = 0; i < index; i++) {
-                    var str;
-                    if (i == 0) {
-                        str = '0';
-                    } else {
-                        str = i;
-                    }
-
-                    var download = hostId + "_download_" + str;
-                    var checkId = hostId + "_install_" + str;
-                    if ($('#' + download).is(':checked')) {
-
-                        var module = $('#' + download).val().replace(/(\r\n|\n|\r)/gm, "");
-                        var mod = baseName(module);
-                        if (mod.toLowerCase().indexOf("nse") >= 0)
-                            options = ' -s nsesoft -b nse1 -u root ';
-                        if (mod.toLowerCase().indexOf("nge") >= 0)
-                            options = ' -s ngesoft -b nge1 -u operator ';
-                        if (mod.toLowerCase().indexOf("nre") >= 0)
-                            options = ' -s nresoft -b nre1 -u root ';
-                        if (mod.toLowerCase().indexOf("nsm") >= 0)
-                            options = ' -s nsmsoft -b nsm1 -u operator ';
-                        if (mod.toLowerCase().indexOf("noe") >= 0)
-                            options = ' -s noesoft -b noe1 -u oracle ';
-                        modules = " -M " + module + " ";
-                        $('#' + checkId).is(':checked') ? install = " -I Yes " : install = " -I No ";
-
-                        hosts.push(host + modules + options + install);
-                        keys.push(input.val());
-
-                    }
-                }
-            });
-            console.log(keys);
-            var unsetAlias = 13;
-            var scriptID = 21;
+            var scriptID = 31; //puppet install script id
             var clientaddress = '<?php echo GlobalConfig::$SYSPROD_SERVER->MGT; ?>';
             var rack = 100;
 
@@ -184,64 +122,24 @@ $this->display('_Header.tpl.php');
             var executionFlag = 1;
             var counter = 1;
             //Ok all values prsed, can we proceed to send to remote servers
-            $.each(hosts, function (index, host) {
-                var argstring = {};
-                var shelf = keys[index];
 
-                argstring = {
-                    "0": host,
-                    "1": " &"
-                };
-                datastring = JSON.stringify(argstring);
-                var number = index + counter;
-                var salesOrder = SO + number;
-                var command = {
-                    salesorder: salesOrder,
-                    rack: rack,
-                    shelf: shelf,
-                    clientaddress: clientaddress,
-                    arguments: datastring,
-                    exesequence: exesequence,
-                    returnstdout: "Waiting for command execution",
-                    executionflag: executionFlag,
-                    scriptid: scriptID
+            datastring = JSON.stringify(args);
 
-                }
-                var Jcommand = JSON.stringify(command);
-                console.log(Jcommand);
-                var url = "/SPOT/provisioning/api/remotecommands/";
-                //Post the remote command to get executed
-                $.ajax({
-                    url: url,
-                    type: "POST",
-                    data: Jcommand,
-                    wait: true,
-                    success: createTR,
-                    error: function (data) {
-                        $('#errormsg').html("An error occured:  " + data.statusText + " " + data.responseText);
-                        console.log(data);
-                        $('#errormsg').show();
-
-                    }
-                }).done(monitoring);
-
-                counter++;
-            });
-            // Send set alias IP command as last command because will be the first to be run
-            var shelf = "Z";
+            var salesOrder = SO;
             var command = {
-                salesorder: SO,
+                salesorder: salesOrder,
                 rack: rack,
-                shelf: shelf,
+                shelf: 'Z',
                 clientaddress: clientaddress,
                 arguments: datastring,
                 exesequence: exesequence,
                 returnstdout: "Waiting for command execution",
                 executionflag: executionFlag,
-                scriptid: ipaliasID
+                scriptid: scriptID
 
             }
             var Jcommand = JSON.stringify(command);
+
             var url = "/SPOT/provisioning/api/remotecommands/";
             //Post the remote command to get executed
             $.ajax({
@@ -249,136 +147,141 @@ $this->display('_Header.tpl.php');
                 type: "POST",
                 data: Jcommand,
                 wait: true,
+                success: createTR,
                 error: function (data) {
                     $('#errormsg').html("An error occured:  " + data.statusText + " " + data.responseText);
                     console.log(data);
                     $('#errormsg').show();
-                    setTimeout(function () {
-                        $('#errormsg').fadeOut(3000);
-                    }, 4000);
+
                 }
-            });
-            function createTR(data) {
-                var commandId = data.remotecommandid;
-                var e = $('<table id="stdout' + commandId + '" class="table-bordered table-responsive table table-striped"></table>');
-                $('#monitorContainer').append(e);
-            }
-            ;
-            function monitoring(data) {
-                setInterval(function () {
+            }).done(monitoring);
 
-                    var commandId = data.remotecommandid;
-                    var url = "/SPOT/provisioning/api/remotecommands/" + commandId
-
-                    $.ajax({
-                        url: url,
-                        type: "GET",
-                        success: function (data) {
-                            var error = data.returncode;
-                            var arguments = data.arguments;
-                            //stdout ID if you want the modal to display
-                            $('#stdout' + commandId).html('<tr><th>Running commandt ' + arguments + '</th></tr><tr><td><pre class="prettyprint">' + data.returnstdout + "</pre><code> " + data.returnstderr + '</code><code>Exit code: ' + error + '</code></pre></td><tr>');
-                        }
-                    });
-                }, 4000);
-            }
-            ;
-            $(document).on('click', '#Results', function (e) {
-                $('#modalMonitor').modal();
-            });
+            counter++;
         });
-    });</script>
+
+
+        function createTR(data) {
+            $('#monitorContainer').show();
+            var commandId = data.remotecommandid;
+            var e = $('<table id="stdout' + commandId + '" class="table-bordered table-responsive table table-striped"></table>');
+            $('#monitorContainer').append(e);
+        }
+        ;
+        function monitoring(data) {
+            setInterval(function () {
+
+                var commandId = data.remotecommandid;
+                var url = "/SPOT/provisioning/api/remotecommands/" + commandId
+
+                $.ajax({
+                    url: url,
+                    type: "GET",
+                    success: function (data) {
+                        var error = data.returncode;
+                        var arguments = data.arguments;
+                        //stdout ID if you want the modal to display
+                        $('#stdout' + commandId).html('<tr><th>Running commandt ' + arguments + '</th></tr><tr><td><pre class="prettyprint">' + data.returnstdout + "</pre><code> " + data.returnstderr + '</code><code>Exit code: ' + error + '</code></pre></td><tr>');
+                    }
+                });
+            }, 4000);
+        }
+        ;
+
+    });
+</script>
 
 <div class="container">
 
     <h1>
-        <i class="icon-th-list"></i> Prepare machines for CCT
+        <i class="icon-th-list"></i> Prepare Puppet server
 
 
     </h1>
     <p id="msg" class="alert alert-error"></p>
     <div class="alert alert-danger" id="errormsg" role="alert" style="display:none"></div>
     <div id="message" class="alert alert-success" role="alert" style="display:none"></div>
-    <!-- underscore template for the collection -->
-
-    <table class="stselection table-bordered table-responsive table table-striped">
-
-        <tr class="salesel">
-            <th>
-                <label for="salesel"><strong>
-                        Select a stored SO
-                    </strong>
-                </label>
-            </th>
-        </tr>
-        <tr class="salesel">
-            <td>
-                <select class="chosen" id="salesel" name="salesel" required autofocus="autofocus">
-                    <option value="">
-                        Select a sales order
-                    </option>
-
-                </select>
-                <div class="pull-right release">
-                    <input type="text"  id="release"  value="" required />&nbsp;<span id="check"> <a href="javascript: void(0)" id="disable"><i class="checkrelease icon-forward "></a></i>Check release path</span>
-
-                </div>
-            </td>
-
-        </tr>
-
-
-
-    </table>
-    <center><div id="relpath" class="alert" hidden></div></center>
-
     <form id="form" role="form" onsubmit="return false;">
+        <table class="stselection table-bordered table-responsive table table-striped">
+
+            <tr class="salesel">
+                <th>
+                    <label for="salesel"><strong>
+                            Select a stored SO
+                        </strong>
+                    </label>
+                </th>
+            </tr>
+            <tr class="salesel">
+                <td>
+                    <select class="chosen" id="salesel" name="salesel" required autofocus="autofocus">
+                        <option value="">
+                            Select a sales order
+                        </option>
+
+                    </select>
+                    <div class="pull-right release">
+                        <input type="text"  id="release"  value="" required />
+
+                    </div>
+                </td>
+
+            </tr>
+
+
+
+        </table>
+
+
+
 
 
         <table class="main table-bordered table-responsive table table-striped">
-            <tr class="network"><th colspan="2">
-            <center>NETWORKING</center>
-            </th></tr>
+            <tr class="network">
+                <th colspan="2">
+            <center>
+                NETWORKING
+            </center>
+            </th>
+            </tr>
 
             <tr class="network">
 
-                <th><label for="ipaddress" class="sr-only" ><b>IP address alias to assign </b></label></th>
+                <th><label for="ipaddress" class="sr-only" ><b>IP address of the puppet candidate </b></label></th>
 
-                <th><label for="netmask"><b>Netmask </b></label></th>
+                <th>
+                    <label for="netmask" class="sr-only"><b>Default Gateway</b></label>
+                </th>
 
             </tr>
             <tr class="network">
 
                 <td>
                     <input name="ipaddress" id="ipaddress" class="form-control ipaddress" type="text" required pattern="((^|\.)((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]?\d))){4}$" />   
+
                 </td>
                 <td>
-                    <input name="netmask" id="netmask" class="form-control netmask" type="text" required pattern="((^|\.)((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]?\d))){4}$" value="255.255.255.0" />
+                    <input name="gateway" id="gateway" class="form-control gateway" type="text" required pattern="((^|\.)((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]?\d))){4}$"  />
                 </td>
-
+               
+            </tr>
+            <tr  class="network">
+                <td colspan="2">
+            <center>
+                <button class='btn btn-primary ' id='install'>Install !</button>
+            </center>
+            </td>
             </tr>
 
         </table>
-
-
     </form>
-    <button class='pull-right btn btn-primary' id='start'>Start !</button>
-    <div class="modal hide fade" id="modalMonitor">
-        <div class="modal-header">
-            <a class="close" data-dismiss="modal">&times;</a>
-            <h3>
-                <i class="icon-road"></i> Running Processes
+    <div id="monitorContainer" hidden>
 
-            </h3>
-        </div>
-        <div class="modal-body">
-            <div id="modelAlert"></div>
-            <div id="monitorContainer"></div>
-        </div>
-        <div class="modal-footer">
-            <button class="btn" data-dismiss="modal" >Cancel</button>
 
-        </div>
+        <h3>
+            <i class="icon-road"></i> Puppet installation 
+
+        </h3>
+
     </div>
 
 </div> <!-- /container -->
