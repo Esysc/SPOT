a0daadbeb0e48d8751a01bb8dc2062cacfc3e230
Andrea Cristalli <acs.cluster@gmail.com>
2016-09-20 16:14:22 +0200

    Created the introduction page

--- index.php
+++ index.php
@@ -5,4 +5,58 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-header('Location: provisioning/index.php');
+//header('Location: provisioning/index.php');
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta charset="utf-8">
+        <title>SPOT System Production Overall Tools selection</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <style type="text/css">
+            body {
+                padding-top: 60px;
+                padding-bottom: 40px;
+            }
+            .sidebar-nav {
+                padding: 9px 0;
+
+            }
+
+        </style>
+
+        <link href="provisioning/bootstrap3/css/bootstrap.css" rel="stylesheet">
+        <script type="text/javascript" src="//code.jquery.com/jquery-2.1.3.js"></script>
+        <script src="provisioning/bootstrap3/js/bootstrap.min.js"></script>
+        <script src="provisioning/scripts/placeholder.js"></script>
+
+
+    </head>
+    <div class='container'>
+        <div class="container-fluid"> 
+
+            <div class="jumbotron">
+                
+                <p class="alert alert-info">System Production Overall Tool Selections</p>
+             
+            </div>
+            <center>
+
+            <a href="provisioning/index.php" class="btn btn-info">Enter the production tool</a>
+             <a href="ipam/index.php" class="btn btn-info">Enter the ipam tool</a>
+                       
+            </center>
+        </div>
+    </div>
+
+
+
+
+
+    
